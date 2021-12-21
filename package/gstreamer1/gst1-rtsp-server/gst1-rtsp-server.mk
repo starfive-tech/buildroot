df80db155f2e9e644ea2703cbad17e7d24a45d97
andy.hu <andy.hu@starfivetech.com>
2021-12-21 18:23:28 +0800

    gst1-rtsp-server: enable example code build for rtsp server feature
    Signed-off-by: andy.hu <andy.hu@starfivetech.com>

--- package/gstreamer1/gst1-rtsp-server/gst1-rtsp-server.mk
+++ package/gstreamer1/gst1-rtsp-server/gst1-rtsp-server.mk
@@ -21,13 +21,13 @@ GST1_RTSP_SERVER_DEPENDENCIES = \
 GST1_RTSP_SERVER_LDFLAGS = $(TARGET_LDFLAGS) $(TARGET_NLS_LIBS)
 
 GST1_RTSP_SERVER_CONF_OPTS = \
-	-Dexamples=disabled \
-	-Dtests=disabled \
+	-Dexamples=enabled \
+	-Dtests=enabled \
 	-Ddoc=disabled
 
 GST1_RTSP_SERVER_CONF_OPTS += \
-	-Dexamples=disabled \
-	-Dtests=disabled \
+	-Dexamples=enabled \
+	-Dtests=enabled \
 	-Dgobject-cast-checks=disabled \
 	-Dglib-asserts=disabled \
 	-Dglib-checks=disabled
