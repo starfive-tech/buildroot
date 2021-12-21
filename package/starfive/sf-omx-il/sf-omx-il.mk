1c902407c9a5328cc0a61b9841d1969228ae7b0a
andy.hu <andy.hu@starfivetech.com>
2021-12-21 18:10:26 +0800

    sf-omx-il: modify sf-omx-il library to support wave521 library
    Signed-off-by: sw.multimedia <sw.multimedia@starfivetech.com>

--- package/starfive/sf-omx-il/sf-omx-il.mk
+++ package/starfive/sf-omx-il/sf-omx-il.mk
@@ -10,9 +10,10 @@ SF_OMX_IL_SITE=$(TOPDIR)/../soft_3rdpart/omx-il
 SF_OMX_IL_SITE_METHOD=local
 SF_OMX_IL_INSTALL_STAGING = YES
 
-SF_OMX_IL_DEPENDENCIES=wave511
+SF_OMX_IL_DEPENDENCIES=wave511 wave521
 define SF_OMX_IL_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) wave521-test
 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) wave511-test
 endef
 
@@ -22,12 +23,15 @@ endef
 
 define SF_OMX_IL_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0644 $(@D)/libsf-omx-il.so $(TARGET_DIR)/usr/lib/libsf-omx-il.so
+	ln -sf libsf-omx-il.so $(TARGET_DIR)/usr/lib/libOMX_Core.so
 	$(INSTALL) -d $(@D)/include/khronos $(TARGET_DIR)/usr/include/omx-il
 	$(INSTALL) -m 0644 $(@D)/include/khronos/* $(TARGET_DIR)/usr/include/omx-il
+	# $(INSTALL) -m 0777 $(@D)/wave521test $(TARGET_DIR)/root/wave521test
 endef
 
 define SF_OMX_IL_INSTALL_STAGING_CMDS
 	$(INSTALL) -m 0644 $(@D)/libsf-omx-il.so $(STAGING_DIR)/usr/lib/libsf-omx-il.so
+	ln -sf libsf-omx-il.so $(STAGING_DIR)/usr/lib/libOMX_Core.so
 	$(INSTALL) -d $(@D)/include/khronos $(STAGING_DIR)/usr/include/omx-il
 	$(INSTALL) -m 0644 $(@D)/include/khronos/* $(STAGING_DIR)/usr/include/omx-il
 endef
