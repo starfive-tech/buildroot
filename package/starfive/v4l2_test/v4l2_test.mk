39af8a61dcc167baedad4788731606bd47004322
Andy Hu <andy.hu@starfivetech.com>
2022-01-12 19:02:06 +0800

    v4l2_test: rename pipeline_setting.sh to media-ctl-pipeline.sh
    Signed-off-by: Andy Hu <andy.hu@starfivetech.com>

--- package/starfive/v4l2_test/v4l2_test.mk
+++ package/starfive/v4l2_test/v4l2_test.mk
@@ -23,7 +23,7 @@ define V4L2_TEST_BUILD_CMDS
 	cp package/starfive/v4l2_test/common.h $(@D)/
 	cp package/starfive/v4l2_test/stf_log.c $(@D)/
 	cp package/starfive/v4l2_test/stf_log.h $(@D)/
-	cp package/starfive/v4l2_test/pipeline_setting.sh $(@D)/
+	cp package/starfive/v4l2_test/media-ctl-pipeline.sh $(@D)/
 	(cd $(@D); $(TARGET_CC) -Wall -O2 v4l2_test.c common.c yuv.c convert.c \
 			stf_v4l2.c stf_framebuffer.c stf_drm.c stf_log.c -lv4l2 -ljpeg \
 			-I$(STAGING_DIR)/usr/include/drm -ldrm -o v4l2test)
@@ -31,7 +31,7 @@ endef
 
 define V4L2_TEST_INSTALL_TARGET_CMDS
 	install -m 0755 -D $(@D)/v4l2test $(TARGET_DIR)/usr/bin/v4l2test
-	install -m 0755 -D $(@D)/pipeline_setting.sh $(TARGET_DIR)/usr/bin/pipeline_setting.sh
+	install -m 0755 -D $(@D)/media-ctl-pipeline.sh $(TARGET_DIR)/usr/bin/media-ctl-pipeline.sh
 endef
 
 V4L2_TEST_DEPENDENCIES = jpeg libv4l
