cf9c62a4f1d08eda1cecbfa1dc271a2cf68361e8
mason.huo <mason.huo@starfivetech.com>
2022-04-15 18:37:37 +0800

    package: Move v4l2test to starfive package
    1.Create starfive package, and move v4l2test into it.
    2.Sync the latest v4l2test package from JH7100.
    
    Signed-off-by: mason.huo <mason.huo@starfivetech.com>
    
    (cherry-pick commit 87583e4004ba7ceefa317d939ea8004f38eb9570 from jh7110-mm-devel branch)

--- package/starfive/v4l2_test/yuv.h
+++ package/starfive/v4l2_test/yuv.h
@@ -4,7 +4,7 @@
 void YUV420toYUV444(int width, int height, unsigned char* src, unsigned char* dst);
 void YUV422toYUV444(int width, int height, unsigned char* src, unsigned char* dst);
 void RGB565toRGB888(int width, int height, unsigned char* src, unsigned char* dst);
-void RAW12toRAW16(int width, int height, unsigned char* src, unsigned char* dst);
-void YUV420NV21toYUV444(int width, int height, unsigned char* src, unsigned char* dst);
-
+void RAW12toRAW16(int width, int height, const unsigned char* src, unsigned char* dst);
+void YUV420NV21toYUV444(int width, int height, unsigned char* src, unsigned char* dst,
+        int is_nv21);
 #endif
