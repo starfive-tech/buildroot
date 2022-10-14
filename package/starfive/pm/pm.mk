f6e892bda3d14c9b634e72ef1abf9f3eaff27f8c
mason.huo <mason.huo@starfivetech.com>
2022-10-14 13:08:20 +0800

    package: starfive: Add power management package
    The package is for power management.
    Currently adjust the cpufreq paramenters.
    
    Signed-off-by: mason.huo <mason.huo@starfivetech.com>

---
+++ package/starfive/pm/pm.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# pm
+#
+################################################################################
+PM_LICENSE = GPL-2.0+
+
+define PM_INSTALL_TARGET_CMDS
+	install -m 0755 -D package/starfive/pm/S90cpufreq $(TARGET_DIR)/etc/init.d/
+endef
+
+$(eval $(generic-package))
