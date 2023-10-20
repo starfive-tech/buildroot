a5e81bba2d9ce7bc78fcdef4cb62ec43367a38a2
Andy Hu <andy.hu@starfivetech.com>
2023-10-20 19:01:21 +0800

    starfive-firmware: Add aic8800 and aic8800DC usb wifi firmware
    aic8800 and aic8800DC support to run on visionfive2 board
    install firmware for aic8800 both 2.4G and 5G
    
    Signed-off-by: Andy Hu <andy.hu@starfivetech.com>
    Signed-off-by: Skyler Zheng <skyler.zheng@starfivetech.com>

--- package/starfive/starfive-firmware/starfive-firmware.mk
+++ package/starfive/starfive-firmware/starfive-firmware.mk
@@ -14,21 +14,21 @@ define STARFIVE_FIRMWARE_INSTALL_AP6256_BLUETOOTH
 endef
 endif
 
-# RTL8852BU Bluetooth
-ifeq ($(BR2_PACKAGE_STARFIVE_FIRMWARE_RTL8852BU_BLUETOOTH),y)
-define STARFIVE_FIRMWARE_INSTALL_RTL8852BU
-	@echo "install RTL8852BU bluetooth firmware ..."
-	$(INSTALL) -m 0755 -D $(STARFIVE_FIRMWARE_PKGDIR)/rtl8852bu-bluetooth/rtl8852bu_config $(TARGET_DIR)/lib/firmware/
-	$(INSTALL) -m 0755 -D $(STARFIVE_FIRMWARE_PKGDIR)/rtl8852bu-bluetooth/rtl8852bu_fw $(TARGET_DIR)/lib/firmware/
-endef
-endif
-
 # AP6256 SDIO Wifi
 ifeq ($(BR2_PACKAGE_STARFIVE_FIRMWARE_AP6256_SDIO_WIFI),y)
 define STARFIVE_FIRMWARE_INSTALL_AP6256_SDIO_WIFI
 	@echo "install AP6256_SDIO_WIFI firmware ..."
 	$(INSTALL) -m 0755 -D $(STARFIVE_FIRMWARE_PKGDIR)/ap6256-sdio-wifi/fw_bcm43456c5_ag.bin $(TARGET_DIR)/lib/firmware/
 	$(INSTALL) -m 0755 -D $(STARFIVE_FIRMWARE_PKGDIR)/ap6256-sdio-wifi/nvram_ap6256.txt $(TARGET_DIR)/lib/firmware/
+endef
+endif
+
+# RTL8852BU Bluetooth
+ifeq ($(BR2_PACKAGE_STARFIVE_FIRMWARE_RTL8852BU_BLUETOOTH),y)
+define STARFIVE_FIRMWARE_INSTALL_RTL8852BU
+	@echo "install RTL8852BU bluetooth firmware ..."
+	$(INSTALL) -m 0755 -D $(STARFIVE_FIRMWARE_PKGDIR)/rtl8852bu-bluetooth/rtl8852bu_config $(TARGET_DIR)/lib/firmware/
+	$(INSTALL) -m 0755 -D $(STARFIVE_FIRMWARE_PKGDIR)/rtl8852bu-bluetooth/rtl8852bu_fw $(TARGET_DIR)/lib/firmware/
 endef
 endif
 
@@ -40,12 +40,24 @@ define STARFIVE_FIRMWARE_INSTALL_ECR6600U
 endef
 endif
 
+# AIC8800 or AIC8800DC USB Wifi
+ifeq ($(BR2_PACKAGE_STARFIVE_FIRMWARE_AIC8800_USB_WIFI),y)
+define STARFIVE_FIRMWARE_INSTALL_AIC8800
+	@echo "install AIC8800 firmware ..."
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/lib/firmware/aic8800
+	$(INSTALL) -m 0644 -t $(TARGET_DIR)/lib/firmware/aic8800 $(STARFIVE_FIRMWARE_PKGDIR)/aic8800-usb-wifi/aic8800/*
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/lib/firmware/aic8800DC
+	$(INSTALL) -m 0644 -t $(TARGET_DIR)/lib/firmware/aic8800DC $(STARFIVE_FIRMWARE_PKGDIR)/aic8800-usb-wifi/aic8800DC/*
+endef
+endif
+
+# install more firmware here
 define STARFIVE_FIRMWARE_INSTALL_TARGET_CMDS
 	$(STARFIVE_FIRMWARE_INSTALL_AP6256_BLUETOOTH)
-	$(STARFIVE_FIRMWARE_INSTALL_RTL8852BU)
 	$(STARFIVE_FIRMWARE_INSTALL_AP6256_SDIO_WIFI)
+	$(STARFIVE_FIRMWARE_INSTALL_RTL8852BU)
 	$(STARFIVE_FIRMWARE_INSTALL_ECR6600U)
-
+	$(STARFIVE_FIRMWARE_INSTALL_AIC8800)
 endef
 
 $(eval $(generic-package))
