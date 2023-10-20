52229a33bc966965375b5fb905cb74a0f71c986e
Andy Hu <andy.hu@starfivetech.com>
2023-10-20 17:30:29 +0800

    starfive-firmware: Add ECR6600U usb wifi firmware
    ECR6600U usb wifi support to run on visionfive2 board
    
    Signed-off-by: Andy Hu <andy.hu@starfivetech.com>

--- package/starfive/starfive-firmware/starfive-firmware.mk
+++ package/starfive/starfive-firmware/starfive-firmware.mk
@@ -7,27 +7,44 @@ STARFIVE_FIRMWARE_LICENSE = Proprietary
 
 # AP6256 Bluetooth
 ifeq ($(BR2_PACKAGE_STARFIVE_FIRMWARE_AP6256_BLUETOOTH),y)
-STARFIVE_FIRMWARE_FILES += $(TOPDIR)/package/starfive/starfive-firmware/ap6256-bluetooth/BCM4345C5.hcd
+define STARFIVE_FIRMWARE_INSTALL_AP6256_BLUETOOTH
+	@echo "install AP6256 BLUETOOTH firmware ..."
+	$(INSTALL) -m 0755 -D $(STARFIVE_FIRMWARE_PKGDIR)/ap6256-bluetooth/BCM4345C5.hcd $(TARGET_DIR)/lib/firmware/
+	$(INSTALL) -m 0755 -D $(STARFIVE_FIRMWARE_PKGDIR)/ap6256-bluetooth/S90bluetooth $(TARGET_DIR)/etc/init.d/
+endef
 endif
 
 # RTL8852BU Bluetooth
 ifeq ($(BR2_PACKAGE_STARFIVE_FIRMWARE_RTL8852BU_BLUETOOTH),y)
-STARFIVE_FIRMWARE_FILES += $(TOPDIR)/package/starfive/starfive-firmware/rtl8852bu-bluetooth/rtl8852bu_config
-STARFIVE_FIRMWARE_FILES += $(TOPDIR)/package/starfive/starfive-firmware/rtl8852bu-bluetooth/rtl8852bu_fw
+define STARFIVE_FIRMWARE_INSTALL_RTL8852BU
+	@echo "install RTL8852BU bluetooth firmware ..."
+	$(INSTALL) -m 0755 -D $(STARFIVE_FIRMWARE_PKGDIR)/rtl8852bu-bluetooth/rtl8852bu_config $(TARGET_DIR)/lib/firmware/
+	$(INSTALL) -m 0755 -D $(STARFIVE_FIRMWARE_PKGDIR)/rtl8852bu-bluetooth/rtl8852bu_fw $(TARGET_DIR)/lib/firmware/
+endef
 endif
-
 
 # AP6256 SDIO Wifi
 ifeq ($(BR2_PACKAGE_STARFIVE_FIRMWARE_AP6256_SDIO_WIFI),y)
-STARFIVE_FIRMWARE_FILES += $(TOPDIR)/package/starfive/starfive-firmware/ap6256-sdio-wifi/fw_bcm43456c5_ag.bin
-STARFIVE_FIRMWARE_FILES += $(TOPDIR)/package/starfive/starfive-firmware/ap6256-sdio-wifi/nvram_ap6256.txt
+define STARFIVE_FIRMWARE_INSTALL_AP6256_SDIO_WIFI
+	@echo "install AP6256_SDIO_WIFI firmware ..."
+	$(INSTALL) -m 0755 -D $(STARFIVE_FIRMWARE_PKGDIR)/ap6256-sdio-wifi/fw_bcm43456c5_ag.bin $(TARGET_DIR)/lib/firmware/
+	$(INSTALL) -m 0755 -D $(STARFIVE_FIRMWARE_PKGDIR)/ap6256-sdio-wifi/nvram_ap6256.txt $(TARGET_DIR)/lib/firmware/
+endef
+endif
+
+# ECR6600U USB Wifi
+ifeq ($(BR2_PACKAGE_STARFIVE_FIRMWARE_ECR6600U_USB_WIFI),y)
+define STARFIVE_FIRMWARE_INSTALL_ECR6600U
+	@echo "install ECR6600U firmware ..."
+	$(INSTALL) -m 0755 -D $(STARFIVE_FIRMWARE_PKGDIR)/ECR6600U-usb-wifi/ECR6600U_transport.bin $(TARGET_DIR)/lib/firmware/
+endef
 endif
 
 define STARFIVE_FIRMWARE_INSTALL_TARGET_CMDS
-	$(foreach f,$(STARFIVE_FIRMWARE_FILES), \
-		$(INSTALL) -m 0755 -D $(f) $(TARGET_DIR)/lib/firmware/; \
-	)
-	$(INSTALL) -D -m 755 package/starfive/starfive-firmware/ap6256-bluetooth/S90bluetooth $(TARGET_DIR)/etc/init.d/S90bluetooth
+	$(STARFIVE_FIRMWARE_INSTALL_AP6256_BLUETOOTH)
+	$(STARFIVE_FIRMWARE_INSTALL_RTL8852BU)
+	$(STARFIVE_FIRMWARE_INSTALL_AP6256_SDIO_WIFI)
+	$(STARFIVE_FIRMWARE_INSTALL_ECR6600U)
 
 endef
 
