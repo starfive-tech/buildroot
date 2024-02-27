4ba3fc2ca7e4e111e0190387915577ddc2169ff9
zejian.su <zejian.su@starfivetech.com>
2024-02-27 13:53:03 +0800

    1. Add SDL preview to render the image with the GPU (issue #8516). 2. Fix the bug of reading parameter fail (issue #9209).
    Signed-off-by: zejian.su <zejian.su@starfivetech.com>

--- package/starfive/libcamera-apps/libcamera-apps.mk
+++ package/starfive/libcamera-apps/libcamera-apps.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIBCAMERA_APPS_SITE = https://github.com/starfive-tech/libcamera-apps.git
-LIBCAMERA_APPS_VERSION = ae96f800c39c1a8a27eb390e97ea08062eb925eb
+LIBCAMERA_APPS_VERSION = d88f3b7c9c199d87180b551016bf894bd5196f09
 LIBCAMERA_APPS_SITE_METHOD = git
 LIBCAMERA_APPS_INSTALL_STAGING = YES
 
